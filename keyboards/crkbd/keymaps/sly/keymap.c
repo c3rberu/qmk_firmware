d04176a20e4e0c72037a607f0adef851dffd563f
c3rberu <105650436+c3rberu@users.noreply.github.com>
2022-05-17 08:08:20 -0400

    Update 0.1.3

--- keyboards/crkbd/keymaps/sly/keymap.c
+++ keyboards/crkbd/keymaps/sly/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX,  BP_Z,    BP_Y,    BP_X,    BP_K,    BP_Q,                        BP_COMM, BP_COMM,   BP_G,    BP_H,    BP_F,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX,  MO(1),  KC_SPC,     KC_ENT,  KC_RSFT, XXXXXXX
+                                          XXXXXXX,  MO(1), KC_RSFT,     KC_ENT,  KC_SPC, XXXXXXX
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX, KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        XXXXXXX,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+      XXXXXXX,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,                      KC_BSPC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
